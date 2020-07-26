4cc55e2318d6f912e3ae8fae59e6fa3b27827ecf
piouson <piouson@gmail.com>
2020-07-27 00:02:28 +0100

    fix layout omission

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import applications from "./components/applications";
 import plans from "./components/plans";
 import settings from "./components/settings";
 import profile from "./components/profile";
+import CustomLayout from "./components/layout/CustomLayout";
 
 function App() {
   return (
     <Admin
       dashboard={Dashboard}
+      layout={CustomLayout}
       customRoutes={[
         <Route key="profile" path="/profile" component={profile.show} />,
       ]}
